19f8ee175f4e7c9dbcf0d440a7f9f2103c9a1c18
Mary Thomson <mlthoms@g.clemson.edu>
2017-09-20 16:37:08 -0400

    got the initial drag and drop working.  No C# logic yet

--- GardenPlanner.Web/App_Start/BundleConfig.cs
+++ GardenPlanner.Web/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace GardenPlanner.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-ui-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
