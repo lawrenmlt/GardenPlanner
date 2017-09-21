04885f6825565b3431a427a97a2f37ff99f86f59
Mary Thomson <mlthoms@g.clemson.edu>
2017-09-21 08:50:44 -0400

    Added first few dtos and an enum

---
+++ GardenPlanner.Dto/GridSquareDto.cs
@@ -0,0 +1,21 @@
+﻿using GardenPlanner.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GardenPlanner.Dto
+{
+    public class GridSquareDto
+    {
+        public int Id { get; set; }
+
+        public int VegetableId { get; set; }
+
+        public GridBorderType Border { get; set; }
+
+        public int Color { get; set; }
+
+    }
+}
---
+++ GardenPlanner.Dto/VegetableDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GardenPlanner.Dto
+{
+    public class VegetableDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Height { get; set; } // in ft
+
+        public int GroundSpace { get; set; } // in sq ft
+    }
+}
